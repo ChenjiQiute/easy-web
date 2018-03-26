cdb2806be0fc4ee36ce909239002286e28d87a58
Administator <Administator@ZZMF-20171213UO.lan>
2018-03-26 13:53:37 +0800

    First commit.

---
+++ marriage/src/main/java/com/tyyf/marriage/MarriageApplication.java
@@ -0,0 +1,12 @@
+package com.tyyf.marriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarriageApplication.class, args);
+	}
+}
---
+++ marriage/src/test/java/com/tyyf/marriage/MarriageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tyyf.marriage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarriageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
